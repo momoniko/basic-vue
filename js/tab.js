9a408949c5bd737159d84b52368bf1b787e82e24
莫莫莫 <746231327@qq.com>
2020-10-14 20:29:57 +0800

    练习

---
+++ js/tab.js
@@ -0,0 +1,10 @@
+class Tab {
+    constructor(id) {
+        this.main = document.querySelector(id);
+        this.lis = this.main.querySelectorall('li');
+    }
+} //1.切换功能
+toggleTab() {}
+toggleTab() {}
+toggleTab() {}
+toggleTab() {}
